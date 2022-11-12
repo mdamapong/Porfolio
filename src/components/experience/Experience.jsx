4d56a526873cdbb84389facea801ed759aba83ee
mickey <damapongm@gmail.com>
2022-11-12 12:18:06 -0800

    exp seciton

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
 import './experience.css';
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+
+
+      <div className='container experience__container'>
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+
+        </div>
+        <div className="experience__backend">
+          <article className='experience__details'>
+            <BsPatchCheckFill />
+            <h4>HTML</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+          <article className='experience__details'>
+            <BsPatchCheckFill />
+            <h4>CSS</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+          <article className='experience__details'>
+            <BsPatchCheckFill />
+            <h4>JavaScript</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+          <article className='experience__details'>
+            <BsPatchCheckFill />
+            <h4>Bootstrap</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+          <article className='experience__details'>
+            <BsPatchCheckFill />
+            <h4>Tailwind</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+          <article className='experience__details'>
+            <BsPatchCheckFill />
+            <h4>Next.js</h4>
+            <small className='text-light'>Experienced</small>
+          </article>
+
+        </div>
+      </div>
+
+    </section>
   )
 }
 
