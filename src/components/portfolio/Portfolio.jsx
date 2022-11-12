3037d4ddd27a63131336cbe9f4b13cc40dbebf27
mickey <damapongm@gmail.com>
2022-11-12 14:38:29 -0800

    portfolio cards

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './portfolio.css';
+import IMG1 from '../../assets/portfolio1.png';
 
 const Portfolio = () => {
   return (
@@ -8,6 +9,65 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href='https://github.com' className='btn'>Github</a>
+            <a href='https://animated-pony-a34044.netlify.app/collections' className='btn btn-primary'>Live Demo</a>
+          </div>
+
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href='https://github.com' className='btn'>Github</a>
+            <a href='https://animated-pony-a34044.netlify.app/collections' className='btn btn-primary'>Live Demo</a>
+          </div>
+
+        </article>        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href='https://github.com' className='btn'>Github</a>
+            <a href='https://animated-pony-a34044.netlify.app/collections' className='btn btn-primary'>Live Demo</a>
+          </div>
+
+        </article>        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href='https://github.com' className='btn'>Github</a>
+            <a href='https://animated-pony-a34044.netlify.app/collections' className='btn btn-primary'>Live Demo</a>
+          </div>
+
+        </article>        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href='https://github.com' className='btn'>Github</a>
+            <a href='https://animated-pony-a34044.netlify.app/collections' className='btn btn-primary'>Live Demo</a>
+          </div>
+
+        </article>        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+            <a href='https://github.com' className='btn'>Github</a>
+            <a href='https://animated-pony-a34044.netlify.app/collections' className='btn btn-primary'>Live Demo</a>
+          </div>
 
         </article>
       </div>
