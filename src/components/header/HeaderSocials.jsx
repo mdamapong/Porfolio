06afda9663edad5e3d4010d6217bdfce9fb9b3cd
mickey <damapongm@gmail.com>
2022-11-12 20:52:28 -0800

    rel="noopener noreferrer"

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import {BsLinkedin} from 'react-icons/bs'
-import {FaGithub} from 'react-icons/fa'
-import {AiFillTwitterCircle} from 'react-icons/ai'
+import { BsLinkedin } from 'react-icons/bs'
+import { FaGithub } from 'react-icons/fa'
+import { AiFillTwitterCircle } from 'react-icons/ai'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/mickey-damapong/' target='_blank'><BsLinkedin /></a>
-        <a href='https://github.com/mdamapong' target='_blank'><FaGithub /></a>
-        <a href='https://twitter.com/mdamapong' target='_blank'><AiFillTwitterCircle /></a>
+      <a href='https://www.linkedin.com/in/mickey-damapong/' target='_blank' rel="noopener noreferrer"><BsLinkedin /></a>
+      <a href='https://github.com/mdamapong' target='_blank' rel="noopener noreferrer"><FaGithub /></a>
+      <a href='https://twitter.com/mdamapong' target='_blank' rel="noopener noreferrer"><AiFillTwitterCircle /></a>
 
 
     </div>
