fc6e0325973426be1a1b88c0df554d05b71e39c9
mickey <damapongm@gmail.com>
2022-11-12 20:37:43 -0800

    CTA

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href='#' className='btn'>Download CV</a>
+      <a href='https://docs.google.com/document/d/14xlG3oj5ANyJgsUru4X2LhGaDid6pM_aKBhundSocmM/edit' className='btn'>Download CV</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
