25fb513f8e7fbf47d8f1534ec27563fe3abc7ca0
mickey <damapongm@gmail.com>
2022-11-15 20:13:41 -0800

    fix footer link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,10 +10,10 @@ const Footer = () => {
       {/* <a href="#" className='footer_logo'>MICKEY</a> */}
       <ul className='permalinks'>
         {/* <li><a href="#">Home</a></li> */}
-        <li><a href="about">About</a></li>
-        <li><a href="expeirence">Expeirence</a></li>
-        <li><a href="portfolio">Portfolio</a></li>
-        <li><a href="contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Expeirence</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
 
 
       </ul>
